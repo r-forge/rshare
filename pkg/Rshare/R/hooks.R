d59d0b903f901e1edca73b8ba61c8e72f1cc9473
cfriedem (Charlie Friedemann) <cfriedem@gmail.com>
2012-09-11 20:50:03 +0000

    added documentation for registerRshareHook function added length check and a warning for objType parameter to registerRshareHook
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rshare@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Rshare/R/hooks.R
+++ pkg/Rshare/R/hooks.R
@@ -15,9 +15,27 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#' Register an Rshare Hook
+#'
+#' Hooks can be used to trigger certain actions when an object of a particular type is sent to the Rshare server. 
+#' These hooks are simple and rather powerful, and can be used for a wide variety of purposes allowing for flexible interprocess communication and control between R sessions.
+#'
+#' @section Details: The hook function must have just two formal arguments: \code{obj} and optionally, \code{port}. 
+#' The \code{obj} argument will contain the object sent to the Rshare server, and thus should contain all data (other than the Rshare port) required for the hook function. 
+#' The optional \code{port} argument will be a single integer specifying the Rshare port number. 
+#' If \code{port} is not an argument to \code{hookFunction} it will not be passed to the hook function.
+#'
+#' @param objType name of the type/class of the objects which will trigger the hook
+#' @param hookFunction the hook function. See details.
+#' @param port the Rshare port number
+#' @return invisibly returns the number of server hooks currently registered for objects of type \code{objType}
 #' @export
 registerRshareHook <- function(objType, hookFunction, port=7777) {
 	if (!is.character(objType)) stop("objType must be of type 'character'")
+	if (!identical(length(objType),1L)) {
+		objType <- objType[1]
+		warning("objType had multiple elements, only first element will be used")
+	}
 	if (!is.function(hookFunction)) stop("hookFunction must be a function")
 	hookFunction <- match.fun(hookFunction)
 	
@@ -29,7 +47,7 @@ registerRshareHook <- function(objType, hookFunction, port=7777) {
 	
 	if (identical(status,"closed")) stop(paste("Rshare must be running on port",port,"in order to register a hook"))
 	if (identical(status,"client")) {
-		# send hook object to server and run it there, much like asssign.Rshare
+		# send hook object to server and wait for response, much like assign.Rshare
 		req <- RshareAddHookReq(objType=objType, hookFunction=hookFunction, port=port)
 		sock <- getClientSocketId(port)
 		res <- sendRObj(req, sock, block=TRUE)
